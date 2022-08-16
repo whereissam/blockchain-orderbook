92109f1a32735352fa06215f64ca7cbf269c47f3
Sam <sam135642@gmail.com>
2022-08-16 21:08:14 +0800

    Seed exchange

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,11 +4,30 @@ const { ethers } = require("hardhat")
 async function main () {
   //Fetch contract to deploy
   const Token = await ethers.getContractFactory('Token')
+  const Exchange = await ethers.getContractFactory('Exchange')
+
+  //Fetch accounts
+  const accounts = await ethers.getSigners()
+
+  console.log(`Account fetched:\n${accounts[0].address}\n${accounts[1].address}\n`)
 
   //Deploy
-  const token = await Token.deploy()
-  await token.deployed()
-  console.log(`Token Deploy to: ${token.address}`)
+  const SSS = await Token.deploy('Blockchain token', 'SSS', '1000000')
+  await SSS.deployed()
+  console.log(`SSS Deploy to: ${SSS.address}`)
+
+  const mETH = await Token.deploy('mETH', 'mETH', '1000000')
+  await mETH.deployed()
+  console.log(`mETH Deploy to: ${mETH.address}`)
+
+  const mDAI = await Token.deploy('mDAI', 'mDAI', '1000000')
+  await mDAI.deployed()
+  console.log(`mDAI Deploy to: ${mDAI.address}`)
+
+  const exchange = await Exchange.deploy(accounts[1].address, 10)
+  await exchange.deployed()
+  console.log(`Exchange Deployed to: ${exchange.address}`)
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
