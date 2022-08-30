e8acba91040fdfab70e02929020cd3773d899f62
Sam <sam135642@gmail.com>
2022-08-30 20:35:08 +0800

    Feat : Make orders

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -1,6 +1,7 @@
 import { ethers } from 'ethers'
 import TOKEN_ABI from '../abis/Token.json'
 import EXCHANGE_ABI from '../abis/Exchange.json'
+// import { exchange } from './reducers'
 
 //1. Load provider(Login wallet) - get the connection info
 export const loadProvider = (dispatch) => {
@@ -68,6 +69,11 @@ export const subscribeToEvents = (exchange, dispatch) => {
   exchange.on('Withdraw', (token, user, amount, balance, event) => {
     dispatch({ type: 'TRANSFER_SUCCESS', event })
   })
+
+  exchange.on('Order', (id, user, tokenGet, amountGet, tokenGive, amountGive, timestamp, event) => {
+    const order = event.args
+    dispatch({ type: 'NEW_ORDER_SUCCESS', order, event })
+  })
 }
 
 //Load user balances ( wallet & exchange balances)
@@ -121,6 +127,52 @@ export const transferTokens = async (provider, exchange, transferType, token, am
   } catch (error) {
     console.error(error)
     dispatch({ type: 'TRANSFER_FAIL' })
+  }
+
+}
+
+// Orders ( buy & sell )
+
+export const makeBuyOrder = async (provider, exchange, tokens, order, dispatch) => {
+  // address _tokenGet,
+  // uint256 _amountGet, 
+  // address _tokenGive,
+  // uint256 _amountGive (orderAmount * orderPrice)
+  const tokenGet = tokens[0].address
+  const amountGet = ethers.utils.parseUnits(order.amount, 18)
+  const tokenGive = tokens[1].address
+  const amountGive = ethers.utils.parseUnits((order.amount * order.price).toString(), 18)
+
+  dispatch({ type: 'NEW_ORDER_REQUEST' })
+
+  try {
+    const signer = await provider.getSigner()
+    const transaction = await exchange.connect(signer).makeOrder(tokenGet, amountGet, tokenGive, amountGive)
+    await transaction.Wait()
+  } catch (error) {
+    dispatch({ type: 'NEW_ORDER_FAIL' })
+  }
+
+}
+
+export const makeSellOrder = async (provider, exchange, tokens, order, dispatch) => {
+  // address _tokenGet,
+  // uint256 _amountGet, 
+  // address _tokenGive,
+  // uint256 _amountGive (orderAmount * orderPrice)
+  const tokenGet = tokens[1].address
+  const amountGet = ethers.utils.parseUnits((order.amount * order.price).toString(), 18)
+  const tokenGive = tokens[0].address
+  const amountGive = ethers.utils.parseUnits(order.amount, 18)
+
+  dispatch({ type: 'NEW_ORDER_REQUEST' })
+
+  try {
+    const signer = await provider.getSigner()
+    const transaction = await exchange.connect(signer).makeOrder(tokenGet, amountGet, tokenGive, amountGive)
+    await transaction.Wait()
+  } catch (error) {
+    dispatch({ type: 'NEW_ORDER_FAIL' })
   }
 
 }
